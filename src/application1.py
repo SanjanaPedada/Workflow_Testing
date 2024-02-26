5fbc97d76eb4aebdfe091efacee2c5f6c76f002c
Sanjana Pedada <sanjanapedada@Sanjanas-MacBook-Pro.local>
2024-02-25 22:22:22 -0500

    setup tests

---
+++ src/application1.py
@@ -0,0 +1,26 @@
+def getExponential(a, b):
+    return (a ** b)
+#Demo comment
+
+def testGreaterThan10(x):
+    if x > 10 and x != 10:
+        return True
+    elif x < 10 and x != 10:
+        return False
+    elif x == 10:
+        return None
+
+
+def getSquared(a):
+    if not isinstance(a, list):
+        raise ValueError("Can only handle lists!")
+    
+    squared_list = []
+    for element in a:
+        squared_list.append(element**2)
+    return squared_list
+
+if __name__ == "__main__":
+    getSquared([1,2])
+    testGreaterThan10(9)
+    getExponential(2,3)
